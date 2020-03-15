6b88ba94966f6d9f440cf4512604175f85d45501
Kirill <k.s.shirokov@mail.ru>
2020-03-15 13:34:09 +0300

    Updated headers

--- project/include/ShipPlacement.h
+++ project/include/ShipPlacement.h
@@ -1,1 +1,15 @@
 #pragma once
+
+enum class Ships {
+    fire = -1,
+    water = 0,
+    small = 1,
+    middle = 2,
+    regular = 3,
+    carrier = 4
+};
+
+enum class orientation {
+    horisontal = 0,
+    vertical = 1,
+};
